68c43840a9870597641d9e85fe3bce6fbd1d449f
Ilya Zhukov <ilyazhukovp@gmail.com>
2022-04-05 17:59:46 +0300

    initial commit

---
+++ src/main/java/app/config/AppConfig.java
@@ -0,0 +1,25 @@
+package app.config;
+
+import app.model.Animal;
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "app")
+public class AppConfig {
+
+    @Bean(name = "dog")
+    public Dog dog(){
+        Animal dog = new Dog();
+        return (Dog) dog;
+    }
+
+    @Bean(name = "timer")
+    public Timer timer(){
+        Timer timer = new Timer();
+        return timer;
+    }
+}
